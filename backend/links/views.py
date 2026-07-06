e9e987d28d9819dc18f7c14ebc93092babc337aa
idcare19 <dream1mm113@gmail.com>
2026-07-06 12:31:41 +0530

    feat: implement link creation API

--- backend/links/views.py
+++ backend/links/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+import json
+from django.views.decorators.csrf import csrf_exempt
+from .models import Link
 # Create your views here.
+
+
+@csrf_exempt
 def link_list(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        
+        original_url = data.get('original_url')
+        
+        if not original_url:
+            return JsonResponse({"success": False, "error":"original_url is required"}, status=400)
+        
+        link = Link.objects.create(original_url=original_url, short_code="123456")
+        
+        return JsonResponse({"success": True,"message":"link created successfully","id": link.id,"original_url": link.original_url,"link": link.short_code})
     return JsonResponse({
+        "success": True,
         "message":"linkpoilot api is working fine", 
     })
